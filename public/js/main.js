b259582123ba98b75e5cd6b5181c3ca2d6d226fc
Serje Zork <atzork@gmail.com>
2015-02-06 03:59:02 +0200

    -- валидация формы логина -- запрос Login на сервер, ответ, реакция на ошибку

--- public/js/main.js
+++ public/js/main.js
@@ -37,46 +37,76 @@ var app = angular.module("app",['ngMessages']);
 //  }
 //}]);
 
-app.directive('loginValidator',function() {
+//app.directive('loginValidator',function() {
+//  return {
+//    restrict: 'AE',
+//    require : 'ngModel',
+//    link    : function($scope,elem,attrs,ngModel) {
+//      ngModel.$validators.login = function(modelValue,viewValue) {
+//        var value = modelValue || viewValue;
+//        var result;
+//        if(!value){
+//          return;
+//        }
+//        result = /^[a-zA-Z0-9]+$/.test(value);
+//        if(result){
+//          $scope.validationError = false;
+//        } else {
+//          $scope.validationError = true;
+//        }
+//        return result;
+//      }
+//    }
+//  }
+//});
+
+app.directive('emailValidation',function(){
   return {
     restrict: 'AE',
     require : 'ngModel',
-    link    : function($scope,elem,attrs,ngModel) {
-      ngModel.$validators.login = function(modelValue,viewValue) {
-        var value = modelValue || viewValue;
-        var result;
-        console.log('value',value);
-        if(!value){
-          return;
-        }
-        result = /^[a-zA-Z0-9]+$/.test(value);
-        if(result){
-          $scope.validationError = false;
-        } else {
-          $scope.validationError = true;
-        }
-        return result;
-      }
+    link    : function($scope,elem,attrs,ngModel){
+      ngModel.$validators.emailNotFound = function(){
+        return true;
+      };
     }
-  }  
+  }
 });
 
-//app.constant("validationClassConfig",{
-//  validClass: "has-success",
-//  invalidClass: "has-error"
-//});
-//
-//app.directive("validationClassFor",["validationClassConfig",function(validationClassConfig){
-//
-//
-//}]);
-
 app.controller('authenticateCtrl',['$scope','$http',function($scope,$http){
-  $scope.authAction = function(formData, validity){
+
+  $scope.authPending = false;
+  $scope.authAction = function authAction(formData, validity){
     if(validity){
-      alert('Отправляем:: ' + JSON.stringify(formData));
-      //$http.post('')
+      $scope.authPending = true;
+      $http.post('/login',formData)
+        .success(function(){
+          console.log('success:: ',arguments);
+          $scope.authPending = false;
+          console.log($scope.authPending);
+        })
+        .error(function(answ,status){
+            $scope.authPending = false;
+            console.log($scope.authPending);
+            if(!answ){
+              return false;
+            }
+            if(answ.typeError && (answ.typeError == 1)){
+              $scope.authForm.email.$setValidity('emailNotFound',false);
+            } else {
+              $scope.authForm.email.$setValidity('emailNotFound',true);
+            }
+          console.log('error:: ',arguments);
+        });
     }
+    return false;
+  };
+
+  $scope.forgotPassword = function forgotPassword(email){
+    $scope.forgotPasswordProcess = false;
+    //$http.post('/forgot-password',{email:email})
+    //  .success()
+    //  .error();
+    console.log('forgotPasswordProcess');
     return false;
   }
 }]);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
+var config = require('../config');
 
 router.get('/',function(req,res) {
   res.render('index');
@@ -21,27 +22,36 @@ router.get('/logout',function(req,res) {
 
 router.post('/login', function(req,res,next){
   console.log('POST - login');
-  passport.authenticate('local',function(err,user,info){
-    console.log('passport.authenticate',arguments);
-    if(err){
-      console.error('Ошибка аутентификации!!!!');
-      return next(err);
-    }
-    if(!user){
-      console.log('Не известный пользователь!!!');
-      req.session.messages = [info.message];
-      return res.redirect('/login');
-    }
-    console.log('Производим Логин..');
-    req.logIn(user,function(err) {
-      if(err){
-        console.log('Ошибка фукнции logIn()');
+  setTimeout(function() {
+    passport.authenticate('local', function (err, user, info) {
+      console.log('passport.authenticate', arguments);
+      if (err) {
+        console.error('Ошибка аутентификации!!!!');
         return next(err);
       }
-      console.log('Получилось залогинится!! пользователь ',user);
-      return res.redirect('/');
-    })
-  })(req,res,next);
+      if (!user) {
+        console.log('Не известный пользователь!!!');
+        req.session.messages = [info.message];
+        res
+            .set({'Content-Type': 'application/json; charset=utf-8'})
+            .status(401)
+            .send({
+              typeError: config.get('errorStatus:emailNotFound:typeError'),
+              message: config.get('errorStatus:emailNotFound:message')
+            });
+        return;
+      }
+      console.log('Производим Логин..');
+      req.logIn(user, function (err) {
+        if (err) {
+          console.log('Ошибка фукнции logIn()');
+          return next(err);
+        }
+        console.log('Получилось залогинится!! пользователь ', user);
+        return res.redirect('/');
+      })
+    })(req, res, next)
+  },4000);
 });
 
 module.exports = router;
