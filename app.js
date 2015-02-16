b4e981d067c9c7fc4250694296d568e90cedda1d
Sergey Zork <atzork@gmail.com>
2015-02-16 19:27:58 +0200

    настройка Gulp и eslint

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /**
  * Created by Zork on 2/2/15.
  */
+'use strict';
 var express = require('express');
-var path    = require("path");
+var path = require('path');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var methodOverride = require('method-override');
@@ -17,36 +18,37 @@ var config = require('./oz.configs/env');
 
 var sessionStore = require('./oz.configs/sessionStore');
 
-var pass = require('./oz.configs/passport');
+require('./oz.configs/passport');
 var passport = require('passport');
 
 var app = express();
 
-var server  = app.listen(config.get('server:port'),function(){
+var server;
+server = app.listen(config.get('server:port'), function () {
   console.log('Start ' + server.address().port);
 });
 
-app.set('views',path.join(__dirname,'oz.public'));
-app.engine('html',require('ejs').renderFile);
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'oz.public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride());
 app.use(sessionStore);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname,'oz.public')));
-app.use(express.static(path.join(__dirname,'node_modules')));
+app.use(express.static(path.join(__dirname, 'oz.public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-app.use('*',allRoutes);
-app.use('/api',apiRoutes);
-app.use('/',routes);
+app.use('*', allRoutes);
+app.use('/api', apiRoutes);
+app.use('/', routes);
 
-app.use(function(req,res,next){
+app.use(function (req, res) {
   res.render('index.html');
 });
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sergey on 2/16/15.
+ */
+var gulp = require("gulp");
+
+gulp.task("default", function() {
+
+});
+
+gulp.task("lint", function() {
+
+});
