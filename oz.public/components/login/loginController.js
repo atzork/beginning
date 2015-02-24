83aa0a27b0e4334967ccc090646c401c3a6a72c6
Sergey Zork <atzork@gmail.com>
2015-02-24 14:03:56 +0200

    форматирование AuthenticateCtrl

--- oz.public/components/login/loginController.js
+++ oz.public/components/login/loginController.js
@@ -8,63 +8,60 @@ var oz = oz || {};
 // oz.authenticateCtrl
 oz.controller('AuthenticateCtrl', ['$rootScope', '$scope', '$http', '$location',
   function($rootScope, $scope, $http, $location) {
-  $rootScope.pageClass = 'login-box';
-  $rootScope.isNoHeader = true;
-  $scope.authPending = false;
-  $scope.authAction = function authAction(formData, validity) {
-    if (validity) {
-      $scope.authPending = true;
-      $http.post('/api/login', formData)
-        .success(function() {
-          $scope.authPending = false;
-          $location.path('/dashboard');
-        })
-        .error(function(answ) {
-          $scope.authPending = false;
-          if (!answ) {
-            return false;
-          }
-
-          for (var att in $scope.authForm.$error) {
-            if ($scope.authForm.$error.hasOwnProperty(att)) {
-              $scope.authForm.email.$setValidity(att, true);
+    $rootScope.pageClass = 'login-box';
+    $rootScope.isNoHeader = true;
+    $scope.authPending = false;
+    $scope.authAction = function authAction(formData, validity) {
+      if (validity) {
+        $scope.authPending = true;
+        $http.post('/api/login', formData)
+          .success(function() {
+            $scope.authPending = false;
+            $location.path('/dashboard');
+          })
+          .error(function(answ) {
+            $scope.authPending = false;
+            if (!answ) {
+              return false;
             }
-          }
-
-          $scope.authForm.$setPristine(true);
-          //$scope.authForm.email.$setValidity('emailNotFound', true);
-
-          //alert();
-
-          $scope.allowSubmit = false;
-          if (answ.typeError) {
-            switch (parseInt(answ.typeError, 10)) {
-              case 1:
-                $scope.authForm.email.$setValidity('emailNotFound', false);
-                $scope.allowSubmit = true;
-                break;
-              case 2:
-                $scope.authForm.password.$setValidity('wrongPassword', false);
-                $scope.allowSubmit = true;
-                break;
-              default:
-                break;
+            for (var att in $scope.authForm.$error) {
+              if ($scope.authForm.$error.hasOwnProperty(att)) {
+                $scope.authForm.email.$setValidity(att, true);
+              }
             }
-          } // if error from server
+            $scope.authForm.$setPristine(true);
+            //$scope.authForm.email.$setValidity('emailNotFound', true);
+            //alert();
+            $scope.allowSubmit = false;
+            if (answ.typeError) {
+              switch (parseInt(answ.typeError, 10)) {
+                case 1:
+                  $scope.authForm.email.$setValidity('emailNotFound', false);
+                  $scope.allowSubmit = true;
+                  break;
+                case 2:
+                  $scope.authForm.password.$setValidity('wrongPassword', false);
+                  $scope.allowSubmit = true;
+                  break;
+                default:
+                  break;
+              }
+            } // if error from server
 
-          console.log('error:: ', arguments);
-        });
-    }
-    return false;
-  };
+            console.log('error:: ', arguments);
+          });
+      }
+      return false;
+    };
 
-  $scope.forgotPassword = function forgotPassword(email) {
-    $scope.forgotPasswordProcess = false;
-    console.log(email);
-    //$http.post('/forgot-password',{email:email})
-    //  .success()
-    //  .error();
-    console.log('forgotPasswordProcess');
-    return false;
-  };
-}]);
+    $scope.forgotPassword = function forgotPassword(email) {
+      $scope.forgotPasswordProcess = false;
+      console.log(email);
+      //$http.post('/forgot-password',{email:email})
+      //  .success()
+      //  .error();
+      console.log('forgotPasswordProcess');
+      return false;
+    };
+  }
+]);
